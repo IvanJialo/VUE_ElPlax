a73a962b62781d58da6af4692333368eb57c6311
Kiwi <ivajimalo@alu.edu.gva.es>
2025-01-23 17:00:59 +0100

    dashboard funcional

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,37 +1,55 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import { getEstudiantes, getEmpresas, getAsignaciones } from '@/composables/useDatabase';
+
+const { fetchEstudiantes } = getEstudiantes();
+const { fetchEmpresas } = getEmpresas();
+const { fetchAsignaciones } = getAsignaciones();
+
+const estudiantes = ref(0);
+const empresas = ref(0);
+const asignaciones = ref(0);
+
+onMounted(async () => {
+  const [estudiantesRes, empresasRes, asignacionesRes] = await Promise.all([
+    fetchEstudiantes(),
+    fetchEmpresas(),
+    fetchAsignaciones(),
+  ]);
+
+  estudiantes.value = estudiantesRes.rows.length;
+  empresas.value = empresasRes.rows.length;
+  asignaciones.value = asignacionesRes.rows.length;
+});
+
+</script>
 <template>
     <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
         <div class="mx-auto max-w-3xl text-center">
-            <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl">Trusted by eCommerce Businesses</h2>
+            <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl">Dashboard</h2>
 
             <p class="mt-4 text-gray-500 sm:text-xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores laborum labore
-                provident impedit esse recusandae facere libero harum sequi.
+                ¡Aquí tienes un pequeño resumen de los datos almacenados en la base de datos!
             </p>
         </div>
 
-        <dl class="mt-6 grid grid-cols-1 gap-4 sm:mt-8 sm:grid-cols-2 lg:grid-cols-4">
+        <dl class="mt-6 grid grid-cols-1 gap-3 sm:mt-8 sm:grid-cols-2 lg:grid-cols-3">
             <div class="flex flex-col rounded-lg bg-blue-50 px-4 py-8 text-center">
-                <dt class="order-last text-lg font-medium text-gray-500">Total Sales</dt>
+                <dt class="order-last text-lg font-medium text-gray-500">Total Alumnos</dt>
 
-                <dd class="text-4xl font-extrabold text-blue-600 md:text-5xl">$4.8m</dd>
+                <dd class="text-4xl font-extrabold text-blue-600 md:text-5xl">{{ estudiantes }}</dd>
             </div>
 
             <div class="flex flex-col rounded-lg bg-blue-50 px-4 py-8 text-center">
-                <dt class="order-last text-lg font-medium text-gray-500">Official Addons</dt>
+                <dt class="order-last text-lg font-medium text-gray-500">Total Empresas</dt>
 
-                <dd class="text-4xl font-extrabold text-blue-600 md:text-5xl">24</dd>
+                <dd class="text-4xl font-extrabold text-blue-600 md:text-5xl">{{ empresas }}</dd>
             </div>
 
             <div class="flex flex-col rounded-lg bg-blue-50 px-4 py-8 text-center">
-                <dt class="order-last text-lg font-medium text-gray-500">Total Addons</dt>
+                <dt class="order-last text-lg font-medium text-gray-500">Total Asignaciones</dt>
 
-                <dd class="text-4xl font-extrabold text-blue-600 md:text-5xl">86</dd>
-            </div>
-
-            <div class="flex flex-col rounded-lg bg-blue-50 px-4 py-8 text-center">
-                <dt class="order-last text-lg font-medium text-gray-500">Downloads</dt>
-
-                <dd class="text-4xl font-extrabold text-blue-600 md:text-5xl">86k</dd>
+                <dd class="text-4xl font-extrabold text-blue-600 md:text-5xl">{{ asignaciones }}</dd>
             </div>
         </dl>
     </div>
