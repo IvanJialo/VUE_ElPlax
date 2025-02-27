c062c272b0acfd68c166288fbde4c5129f576c75
Àngel Buigues Andrés <angelbuiguesandres.teulada@gmail.com>
2025-02-27 14:24:51 +0100

    contrasena profesor

--- src/views/EditarProfesorView.vue
+++ src/views/EditarProfesorView.vue
@@ -109,7 +109,7 @@ const guardarProfesor = async () => {
 
                         <div>
                             <label for="contrasena" class="block text-sm font-medium text-gray-700">Contraseña</label>
-                            <input v-model="form.contrasena" type="password" name="contrasena" id="contrasena" required
+                            <input v-model="form.contrasena" disabled type="password" name="contrasena" id="contrasena" 
                                 class="block w-full px-3 py-2 mt-1 text-sm text-gray-900 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm focus:outline-none">
                         </div>
 
